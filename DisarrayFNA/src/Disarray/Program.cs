961e10c169242d6645c72c6d2d1457fc3afcf60c
lunkums <lukeleedy123@gmail.com>
2023-03-13 21:18:41 -0400

    Add test scene

---
+++ DisarrayFNA/src/Disarray/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Disarray;
+
+
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        using (Game1 game = new Game1())
+        {
+            game.Run();
+        }
+    }
+}
